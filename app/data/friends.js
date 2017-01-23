208e27e5402c8fccc44767ae8650cc80e15a4184
Jeff Swearingen <jeffswearingen@gmail.com>
2017-01-23 11:04:28 -0500

    added friends data

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+var profiles = [{
+	"name"   : "George Washington",
+	"photo"  : "",
+	"values" : [1, 2, 3, 4, 5, 4, 3, 2, 1, 3]
+   },
+   {"name"   : "John Adams",
+    "photo"  : "",
+    "values" : [5, 4, 3, 2, 5, 4, 3, 5, 5, 1]
+   },
+   {"name"   : "Thomas Jefferson",
+    "photo"  : "",
+    "values" : [3, 3, 3, 3, 1, 2, 1, 1, 5, 4]
+   },
+   {"name"   : "James Madison",
+    "photo"  : "",
+    "values" : [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+   },
+   {"name"   : "James Monroe",
+    "photo"  : "",
+    "values" : [2, 2, 2, 5, 5, 5, 2, 2, 5, 2]
+   },
+   {"name"   : "John Quincy Adams",
+    "photo"  : "",
+    "values" : [3, 2, 3, 2, 4, 3, 4, 3, 5, 4]
+   },
+   {"name"   : "Andrew Jackson",
+    "photo"  : "",
+    "values" : [5, 5, 5, 5, 5, 4, 5, 4, 5, 4]
+   },
+   {"name"   : "Martin Van Buren",
+    "photo"  : "",
+    "values" : [3, 2, 5, 4, 3, 2, 1, 5, 4, 3]
+   },
+   {"name"   : "William Henry Harrison",
+    "photo"  : "",
+    "values" : [1, 2, 1, 2, 1, 1, 1, 1, 2, 1]
+   },
+   {"name"   : "John Tyler",
+    "photo"  : "",
+    "values" : [5, 5, 5, 3, 3, 3, 2, 2, 2, 2]
+}];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
 var path = require('path');
 
+var profiles = require('../data/friends.js');
+
 module.exports = function (app) {
 
 	// Express routes
@@ -7,10 +9,12 @@ module.exports = function (app) {
 
 	app.get('/api/friends', function(req, res) {
 		// show all friends
+		res.json(profiles);
 	});
 
 	app.post('/api/friends', function(req, res) {
 		// handle incoming posts, handle compatibility logic
+		
 		console.log(req);
 	});
 
